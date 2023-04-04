90e6f7cb5133dea8cf49bc726cb87c950adf97c0
Pratik Bhagwat <pratikbhagwatdev@gmail.com>
2023-04-04 16:10:01 +0530

    added loading feature

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 "use strict";
 let apiQuotes = [];
 
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 const quoteContainer = document.querySelector("#quote-container");
 const quoteText = document.querySelector("#quote");
 const authorText = document.querySelector("#author");
 const twitterBtn = document.querySelector("#twitter");
 const newQuoteBtn = document.querySelector("#new-quote");
+const loader = document.querySelector("#loader");
 
 // Show new Quote
 function newQuote() {
+  loading();
   // pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -17,10 +29,12 @@ function newQuote() {
   if (quote.text.length > 50) quoteText.classList.add("long-quote");
   else quoteText.classList.remove("long-quote");
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // get Quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     //await waits until data is fetched then assign it to variable
